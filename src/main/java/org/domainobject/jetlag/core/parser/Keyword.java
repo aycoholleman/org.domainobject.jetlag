c8186701ecbedd7071578f863eac292448ee239a
Ayco Holleman <ayco_holleman@yahoo.com>
2015-05-01 23:24:18 +0200

    Added KeyWord enum

---
+++ src/main/java/org/domainobject/jetlag/core/parser/Keyword.java
@@ -0,0 +1,56 @@
+package org.domainobject.jetlag.core.parser;
+
+import java.util.HashMap;
+
+/**
+ * @author ayco
+ * @created May 1, 2015
+ *
+ */
+public enum Keyword
+{
+	NULL, NOT, AND, OR, TRUE, FALSE, EITHER, NEITHER;
+
+	private static final HashMap<String, Keyword> table = new HashMap<>(10, 1F);
+
+	static {
+		for (Keyword kw : values()) {
+			table.put(kw.word, kw);
+		}
+	}
+
+
+	/**
+	 * Get the enum value corresponding to the specified keyword.
+	 * 
+	 * @param keyword
+	 *            The {@code String} to lookup
+	 * @return The corresponding {@code KeyWord}, or {@code null} if the
+	 *         argument pased to this method is not a keyword.
+	 */
+	public static Keyword lookup(String keyword)
+	{
+		return table.get(keyword);
+	}
+
+	private final String word;
+
+
+	public String toString()
+	{
+		return word;
+	}
+
+
+	private Keyword()
+	{
+		this.word = name().toLowerCase();
+	}
+
+
+	private Keyword(String word)
+	{
+		this.word = word;
+	}
+
+}
