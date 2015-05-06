9525a9468a6e580d1d76da4a10dfae8848d106f3
Ayco Holleman <ayco_holleman@yahoo.com>
2015-05-06 21:41:54 +0200

    New stuff

---
+++ src/main/java/org/domainobject/jetlag/core/funclib/AbstractFunctionLibrary.java
@@ -0,0 +1,14 @@
+package org.domainobject.jetlag.core.funclib;
+
+import org.domainobject.jetlag.core.runtime.TemplateRunner;
+
+/**
+ * @author ayco
+ * @created May 6, 2015
+ *
+ */
+public abstract class AbstractFunctionLibrary {
+	
+	protected TemplateRunner templateRunner;
+
+}
---
+++ src/main/java/org/domainobject/jetlag/core/runtime/TemplateRunner.java
@@ -0,0 +1,11 @@
+package org.domainobject.jetlag.core.runtime;
+
+/**
+ * @author ayco
+ * @created May 6, 2015
+ *
+ */
+public abstract class TemplateRunner {
+
+	
+}
