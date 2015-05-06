1064396951dfa53bf957853078e257bc8e7795cd
Ayco Holleman <ayco.holleman@naturalis.nl>
2015-05-06 15:00:09 +0200

    Started on funclib

---
+++ src/main/java/org/domainobject/jetlag/core/funclib/FunctionLibrary.java
@@ -0,0 +1,14 @@
+package org.domainobject.jetlag.core.funclib;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * @author ayco
+ * @created May 6, 2015
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FunctionLibrary {
+
+}
---
+++ src/main/java/org/domainobject/jetlag/core/funclib/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author ayco
+ *
+ */
+package org.domainobject.jetlag.core.funclib;
