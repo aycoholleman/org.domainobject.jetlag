3d1793096910aa8241afce552b2f4791ea584e41
Ayco Holleman <ayco.holleman@naturalis.nl>
2015-05-06 08:39:23 +0200

    Added unit test and started work on parser

---
+++ src/main/java/org/domainobject/jetlag/core/parser/expression/AbstractExpression.java
@@ -0,0 +1,10 @@
+package org.domainobject.jetlag.core.parser.expression;
+
+public abstract class AbstractExpression {
+
+	public AbstractExpression()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/test/java/org/domainobject/jetlag/core/parser/tokenizer/SingleQuotedStringTokenTest.java
@@ -0,0 +1,22 @@
+package org.domainobject.jetlag.core.parser.tokenizer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SingleQuotedStringTokenTest {
+
+	@Test
+	public void testGetType()
+	{
+		fail("Not yet implemented");
+	}
+
+
+	@Test
+	public void testExtract()
+	{
+		fail("Not yet implemented");
+	}
+
+}
